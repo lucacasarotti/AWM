b1dd36978b674ac02b8b44e76c2eb5a064dfdb9e
lucacasarotti <lucacasarotti96@gmail.com>
2021-03-17 15:57:31 +0100

    non_funziona_ancora

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
 
 from . import views
+from .api import MessageModelViewSet, UserModelViewSet
+
+router = DefaultRouter()
+router.register(r'message', MessageModelViewSet, basename='message-api')
+router.register(r'user', UserModelViewSet, basename='user-api')
+
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path(r'<str:room_name>/api/v1/', include(router.urls)),
+    #path('<str:room_name>/', views.room, name='room'),
+    path('<str:room_name>/', login_required(
+        TemplateView.as_view(template_name='chatroom/chat.html')), name='home'),
 ]
